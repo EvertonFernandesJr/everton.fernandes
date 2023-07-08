0e78d925e5ce52620a90c747fdd4514eb41305da
Lucas Peixoto <lspeixotodev@gmail.com>
2023-07-08 16:00:13 -0300

    Deploy v1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'profile_v2' title`, () => {
+  it(`should have the 'resume' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('profile_v2');
+    expect(app.title).toEqual('resume');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('profile_v2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('resume app is running!');
   });
 });
